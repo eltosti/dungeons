0b5d8410a366c8fd2ad4ae66da78298a49c4560c
pablo <eltosti@gmail.com>
2020-04-03 12:54:22 -0300

    change from extends block to extend faccing block, fix jailbars texture

--- src/main/java/net/dungeons/WallBlock.java
+++ src/main/java/net/dungeons/WallBlock.java
@@ -2,17 +2,26 @@ package net.dungeons;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.FacingBlock;
 import net.minecraft.entity.EntityContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.IntProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.state.property.Property;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 
-public class WallBlock extends Block {
+
+public class WallBlock extends FacingBlock {
     public WallBlock(Settings settings)
     {
         super(settings);
     }
+
+    net.minecraft.block.BlockState
 
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, EntityContext ctx) {
